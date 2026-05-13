ce5c55e16881cdae6c9374bfa922447ad1a6301d
AidaG91 <aida.garmus@gmail.com>
2026-05-13 18:13:02 +0200

    fix: broken links fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import { Outlet } from 'react-router-dom';
-import Header from "./components/header/Header";
-
-// 1. Borramos o comentamos el Footer porque el log indica que está ahí y quizás no exista aún
-// import Footer from "./components/footer/Footer"; 
-
-// 2. CAMBIO CLAVE: Cambiamos .css por .scss o borramos la línea si ya lo importas en main.scss
-import "./App.scss"; 
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 export default function App() {
   return (
-    <div className="App">
+    <>
       <Header />
+
       <main>
-        {/* El Outlet es donde se verán tus páginas como Landing */}
-        <Outlet /> 
+        <Outlet />
       </main>
-      {/* <Footer /> */}
-    </div>
+
+      <Footer />
+    </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./style/main.scss";
+import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./config/router";
-// Importación de Sass
-import "./style/main.scss"; 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
