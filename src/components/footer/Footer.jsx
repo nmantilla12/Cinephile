494bbff87a5e1ce4feb22cd11c65bb7e08a2ee5a
AidaG91 <aida.garmus@gmail.com>
2026-05-13 15:04:47 +0200

    feat: añadir maquetacion html a footer. Añadir codigo básico a páginas jsx para evitar error

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,28 @@
+import styles from "./Footer.module.scss";
+import worldIcon from "../../assets/images/icons/world.png";
+import shareIcon from "../../assets/images/icons/share.png"
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <>
+      <footer>
+        <h3>CINEPHILE</h3>
+        <div>
+          <h4>Privacy Policy</h4>
+          <h4>Terms of Service</h4>
+          <h4>Help Center</h4>
+          <h4>API</h4>
+        </div>
+        <div>
+          <p>&copy;2024 Cinephile. Precision Engineered Streaming</p>
+        </div>
+        <div>
+          <img
+            src={worldIcon} alt="world icon"
+          />
+          <img src={shareIcon} alt="share page icon" />
+        </div>
+      </footer>
+    </>
+  );
 }
