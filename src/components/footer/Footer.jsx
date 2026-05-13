bec6e6ee9d38c50a3d7ab8601d44581573413b66
AidaG91 <aida.garmus@gmail.com>
2026-05-13 16:04:02 +0200

    style: añadir estilos al footer tanto en mobile como en desktop

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,25 +1,35 @@
 import styles from "./Footer.module.scss";
 import worldIcon from "../../assets/images/icons/world.png";
-import shareIcon from "../../assets/images/icons/share.png"
+import shareIcon from "../../assets/images/icons/share.png";
 
 export default function Footer() {
   return (
     <>
-      <footer>
-        <h3>CINEPHILE</h3>
-        <div>
-          <h4>Privacy Policy</h4>
-          <h4>Terms of Service</h4>
-          <h4>Help Center</h4>
-          <h4>API</h4>
+      <footer className={styles.footer}>
+        <div className={styles.title}>
+          <h3 className={styles.logo}>CINEPHILE</h3>
+          <p className={styles.subtitle}>Precision Engineered Streaming.</p>
         </div>
-        <div>
-          <p>&copy;2024 Cinephile. Precision Engineered Streaming</p>
+
+        <div className={styles.links}>
+          <div className={styles.linksTop}>
+            <p>Privacy Policy</p>
+            <p>Terms of Service</p>
+          </div>
+          <div className={styles.linksBottom}>
+            <p>Help Center</p>
+            <p>API</p>
+          </div>
         </div>
-        <div>
-          <img
-            src={worldIcon} alt="world icon"
-          />
+        <div className={styles.copy}>
+          <p>
+            <span className={styles.firstPart}>&copy;2024 Cinephile. </span>
+            Precision Engineered Streaming.
+          </p>
+        </div>
+
+        <div className={styles.icons}>
+          <img src={worldIcon} alt="world icon" />
           <img src={shareIcon} alt="share page icon" />
         </div>
       </footer>
