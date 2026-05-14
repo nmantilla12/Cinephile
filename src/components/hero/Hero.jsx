46f3994335c50e0d58618efc65714dc4e281e1b0
Johanna Monroy <johamonroy20@gmail.com>
2026-05-14 18:12:10 +0200

    style:estilo en hero-badge-maintitle

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,9 +3,14 @@ export default function Hero() {
   return (
     <>
       <section className={style.heroSection}>
-        <h4 className={style.badge}>ULTRA HD sTREAMING</h4>
-        <h1 className={style.mainTitle}>Discover <span>Pure</span> Cinema.</h1>
-        <p className={style.description}>Precision-engineered streaming for the world's most dedicated cinephiles.</p>
+        <h4 className={style.badge}>ULTRA HD TREAMING</h4>
+        <h1 className={style.mainTitle}>
+          Discover <span>Pure</span> Cinema.
+        </h1>
+        <p className={style.description}>
+          Precision-engineered streaming for the world's most dedicated
+          cinephiles.
+        </p>
       </section>
     </>
   );
