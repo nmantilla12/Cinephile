ca83035da9c548120abc214037db985cf9db8f1a
Johanna Monroy <johamonroy20@gmail.com>
2026-05-15 11:44:49 +0200

    feat:Crear seccion contenido en LandingPage

---
+++ src/pages/landingpage/ContentSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Hero from "../../components/Hero/Hero";
+import style from "./ContentSection.module.scss";
+
+import portada from "../../assets/images/landingpage/portada-m-d.png";
+
+import neoTokyo from "../../assets/images/landingpage/neo-tokyo-mobile.png";
+
+export default function ContentSection() {
+    return()
+} 
--- src/pages/landingpage/Landing.jsx
+++ src/pages/landingpage/Landing.jsx
@@ -4,6 +4,7 @@ export default function Landing() {
   return (
     <>
       <Hero />
+      
     </>
   );
 }
